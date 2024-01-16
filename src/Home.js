2bccd57e27e280f30f2257148cb9c76ed6af0751
Andresmarevi <andresmarevi@gmail.com>
2024-01-16 18:17:07 +0100

    FixAlerts

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
+import AnimalGame from './AnimalGame';
+import { useNavigation } from
+ 
+'@react-navigation/native'
+
 
 const Home = () => {
+  const navigation = useNavigation();
   return (
-    <View style={{ flex: 1, backgroundColor: '#F0F8FF' }}>
-      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Choose a game</Text>
+    <View
+ 
+style={{
+ 
+flex:
+ 
+1, backgroundColor: '#4CAF50' }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center' }}>Choose a game</Text>
       <View style={{ flexDirection: 'column', justifyContent: 'center', marginBottom: 20, marginTop: 280 }}>
         <Button title='Animals'
-          onPress={() => alert('Go to Animals!')}
+          onPress={() => navigation.navigate('AnimalGame')}
         />
         <Button title='Maths'
-          onPress={() => alert('Go to Maths')}
+          onPress={() => navigation.navigate('MathGame')}
         />
       </View>
     </View>
