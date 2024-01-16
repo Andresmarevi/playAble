da742fc5cea42a10a338aadb369e1a4f49e1bf0b
Andresmarevi <andresmarevi@gmail.com>
2024-01-16 17:13:45 +0100

    playAble

--- config.js
+++ config.js
@@ -1,28 +1,21 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth'
-import 'firebase/compat/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: "AIzaSyD1ijX1D7sAl9rYPBthIa6Y27uMAWqh9h0",
+  authDomain: "playable-e53d7.firebaseapp.com",
+  projectId: "playable-e53d7",
+  storageBucket: "playable-e53d7.appspot.com",
+  messagingSenderId: "283918241507",
+  appId: "1:283918241507:web:9a1b47a786e8ef71c47fd6",
+  measurementId: "G-N3XT2B8V8N"
+};
 
-    apiKey: "AIzaSyD8OAEBX0886TCF0vWcLOqd1mQu18O8TG4",
-  
-    authDomain: "notesandres-ba5cd.firebaseapp.com",
-  
-    projectId: "notesandres-ba5cd",
-  
-    storageBucket: "notesandres-ba5cd.appspot.com",
-  
-    messagingSenderId: "472473428310",
-  
-    appId: "1:472473428310:web:34514b4662c3557608b6f7",
-  
-    measurementId: "G-KRY711BP3B"
-  
-  };
-
-
-if (!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig);
-}
-
-export {firebase};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
