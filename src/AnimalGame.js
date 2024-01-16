395bd539bf71bb51d7571c059bd20526639215d9
borjaalba <borjaalba@gmail.com>
2024-01-16 17:24:50 +0100

    cambios animal

--- src/AnimalGame.js
+++ src/AnimalGame.js
@@ -1,63 +1,3 @@
-// // JuegoUno.js
-// import React, { useState } from 'react'
-// import { View, Button, StyleSheet, Image, Alert } from 'react-native'
-// import { useNavigation } from '@react-navigation/native'
-
-// const JuegoUno = () => {
-//     const navigation = useNavigation();
-//     const [currentAnimal, setCurrentAnimal] = useState(0);
-
-//     const animals = [
-//         {
-//             image: require('../images/istockphoto-174432616-612x612.jpeg'),
-//             options: ['Gorila', 'Jirafa', 'Andres', 'Javi'],
-//             correctOption: 'Javi'
-//         },
-//         {
-//             image: require('../images/istockphoto-174432616-612x612.jpeg'), // Reemplaza esto con la ruta a tu segunda imagen
-//             options: ['Opción 1', 'Opción 2', 'Opción 3', 'Opción 4'], // Reemplaza esto con tus opciones para el segundo animal
-//             correctOption: 'Opción 1' // Reemplaza esto con la opción correcta para el segundo animal
-//         }
-//         // Puedes agregar más animales aquí...
-//     ];
-
-//     const handleButtonPress = (option) => {
-//         if (option === animals[currentAnimal].correctOption) {
-//             Alert.alert('Correcto!');
-//             setCurrentAnimal(currentAnimal + 1);
-//         } else {
-//             Alert.alert('Incorrecto! Intenta de nuevo.');
-//         }
-//     }
-
-//     return (
-//         <View style={styles.container}>
-//             <Image source={animals[currentAnimal].image} style={styles.image} />
-//             {animals[currentAnimal].options.map((option, index) => (
-//                 <Button key={index} title={option} onPress={() => handleButtonPress(option)} />
-//             ))}
-//             <Button title='Regresar' onPress={ ()=> navigation.goBack()}/>
-//         </View>
-//     )
-// }
-
-// export default JuegoUno
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         backgroundColor: '#fff'
-//     },
-//     image: {
-//         width: 200,
-//         height: 200,
-//         marginBottom: 20
-//     },
-// })
-
-
 import React, { useState } from 'react';
 import { View, Button, StyleSheet, Image, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
@@ -126,16 +66,26 @@ const AnimalGame = () => {
 
     const generateOptions = () => {
         const options = getRandomAnimals(currentAnimal);
-        return options.map((option, index) => (
-            <Button key={index} title={option} onPress={() => handleButtonPress(option)} />
+        const buttons = options.map((option, index) => (
+            <View key={index} style={styles.buttonContainer}>
+                <Button title={option} onPress={() => handleButtonPress(option)} color="white" />
+            </View>
         ));
+        return (
+            <View style={styles.optionsContainer}>
+                <View style={styles.row}>{buttons.slice(0, 2)}</View>
+                <View style={styles.row}>{buttons.slice(2)}</View>
+            </View>
+        );
     };
 
     return (
         <View style={styles.container}>
             <Image source={animalImages[currentAnimal].image} style={styles.image} />
             {generateOptions()}
-            <Button title='Come back' onPress={() => navigation.goBack()} />
+            <View style={styles.buttonContainer}>
+                <Button title='Come back' onPress={() => navigation.goBack()} color="white" />
+            </View>
         </View>
     );
 };
@@ -147,13 +97,29 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#fff'
+        backgroundColor: '#4c00b0'
     },
     image: {
         width: 200,
         height: 200,
-        marginBottom: 20
+        marginBottom: 20,
+        resizeMode: 'contain'
     },
+    optionsContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        width: '80%'
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    buttonContainer: {
+        width: '45%',
+        margin: 10,
+        backgroundColor: 'lightblue',
+        borderRadius: 5
+    }
 });
 
 // Con la fire base
