56ff1779434af8fdd0387ab8d5ee35a7e1cea0f2
Andresmarevi <andresmarevi@gmail.com>
2024-01-17 15:24:30 +0100

    numbersImages

--- App.js
+++ App.js
@@ -68,19 +68,6 @@ export default function App() {
           headerTitleAlign: 'center', // Add this line to center the header title
         }}
       />
-      
-      <Stack.Screen
-          component={users}
-          name='Users'
-          options={{
-            headerTitle: () => <Header name='Login' />,
-            headerStyle: {
-              backgroundColor: '#D3D3D3',
-              height: 120,
-            },
-            headerTitleAlign: 'center', // Add this line to center the header title
-          }}
-        />
         </Stack.Navigator>
     </NavigationContainer>
   )
