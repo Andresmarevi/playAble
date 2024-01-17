9706c135de2c47667474deb5f290b5552f468020
borjaalba <borjaalba@gmail.com>
2024-01-17 10:42:43 +0100

    animales

--- src/styles/animalGameStyle.js
+++ src/styles/animalGameStyle.js
@@ -48,4 +48,12 @@ export const animalGameStyles = StyleSheet.create({
     width: '100%',
     height: 200,
   },
+  video: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: 'transparent',
+  },
 });
